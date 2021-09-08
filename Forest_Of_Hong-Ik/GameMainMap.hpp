0ed9d6366b9adbb850f30cb1326089dd23334069
SeoSungwon <tjtjddnjs8688@gmail.com>
2021-09-08 13:49:25 +0900

    21y 09m 08d

--- Forest_Of_Hong-Ik/GameMainMap.hpp
+++ Forest_Of_Hong-Ik/GameMainMap.hpp
@@ -15,7 +15,7 @@ public:
         constexpr auto game_map_view_size_x = 39;
         constexpr auto game_map_view_size_y = 19;
 
-        // 포인터 클래스 초기화.
+        // 포인터 클래스 초기화
         gameMapManager = new GetGameMapManager(gameMapTextFileName, gameMapDataTextFileName,
             map_top_left_corner_x, map_top_left_corner_y,
             game_map_view_size_x, game_map_view_size_y);
